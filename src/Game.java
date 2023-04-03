70a435d4ab2052de14e5b3bf8db2dc653035344c
Keenan Higgins <khiggins767alt@gmail.com>
2023-04-03 14:18:06 -0400

    Added Player Class

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,32 @@
-//test
+import java.util.ArrayList;
+
+/*
+ * Class name: Die
+ * Purpose: represents a die that can be rolled
+ * Coder: Bayethe Lungah and Keenan
+ * Date: April 3, 2023 
+ */
 
 public class Game
 {
+    private int totalPotAmount;
+    private ArrayList<Player>playerList;
+
+    public Game(){
+
+    }
+
+    public ArrayList<Player> getPlayerList() {
+        return playerList;
+    }
+    public int getTotalPotAmount() {
+        return totalPotAmount;
+    }
+    public void populatePlayerList(){
+
+    }
+    public boolean checForGameWinner(){
+        return true;
+    }
 
 }
