33feb35c3b4b38c3fa7d44d992edf5674222c681
Keenan Higgins <khiggins767alt@gmail.com>
2023-04-03 16:18:58 -0400

    Changes

--- src/Craps.java
+++ src/Craps.java
@@ -6,16 +6,12 @@
  */
 
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-
 import java.awt.*;
 import java.awt.event.*;
 
 public class Craps extends JFrame
 {
-    private JMenuBar navBar;
-    private int numOfPlayers;
+    JMenuBar navBar;
 
     public Craps()
     {
@@ -24,30 +20,10 @@ public class Craps extends JFrame
         this.setLayout(new BorderLayout());
         this.setLocationRelativeTo(null);
 
-        // slider creation inside constructor
-        JSlider slider = new JSlider(JSlider.HORIZONTAL, 2, 6, 2);
-        slider.setMajorTickSpacing(1);
-
-        slider.addChangeListener(new SliderHandler());
-
         navBar = new JMenuBar();
 
-        JMenu instructionsMenu = new JMenu("Instructions");
-        JMenu aboutMenu = new JMenu("About");
+        navBar.setBackground(Color.BLACK);
 
-        aboutMenu.addItemListener((ItemEvent event) ->
-        {
-            if (event.getStateChange() == ItemEvent.SELECTED)
-            {
-                JOptionPane.showMessageDialog(null, "About", "About", JOptionPane.INFORMATION_MESSAGE);
-            }
-
-        });
-
-        navBar.add(instructionsMenu);
-        navBar.add(aboutMenu);
-
-        this.add(slider, BorderLayout.CENTER);
         this.add(navBar, BorderLayout.NORTH);
         this.setVisible(true);
     }
@@ -56,21 +32,4 @@ public class Craps extends JFrame
     {
         new Craps();
     }
-
-    private class SliderHandler implements ChangeListener
-    {
-
-        @Override
-        public void stateChanged(ChangeEvent e)
-        {
-            JSlider source = (JSlider) e.getSource();
-
-            if (!source.getValueIsAdjusting())
-            {
-                numOfPlayers = (int) source.getValue();
-            }
-        }
-
-    }
-
 }
