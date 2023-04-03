65b1077c5a6966ac86beaf94bd3419c63f33e8a0
Keenan Higgins <khiggins767alt@gmail.com>
2023-04-03 15:00:44 -0400

    test2

--- src/Game.java
+++ src/Game.java
@@ -10,22 +10,13 @@ import java.util.ArrayList;
 public class Game
 {
     private int totalPotAmount;
-<<<<<<< HEAD
     private ArrayList<Player>playerList;
 
     public Game() {
         totalPotAmount = 0;
         playerList = new ArrayList<Player>();
-=======
-    private ArrayList<Player> playerList;
-    private ArrayList<Pass> pass;
-
-    public Game()
-    {
-
->>>>>>> 55e753e09e4fae22581671a5d92f1a980477c2a1
     }
-
+    
     public ArrayList<Player> getPlayerList()
     {
         return playerList;
@@ -44,3 +35,4 @@ public class Game
     public boolean checkForGameWinner(){
         return true;
     }
+}
