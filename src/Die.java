e571ecd7d3edc3235b427bc6ecd5b7137a2848cf
Bayethe Lungah <Bayethelungah@gmail.com>
2023-04-03 14:07:39 -0400

    Added die class

---
+++ src/Die.java
@@ -0,0 +1,27 @@
+/*
+ * Class name: Die
+ * Purpose: represents a die that can be rolled
+ * Coder: Bayethe Lungah and Keenan
+ * Date: April 3, 2023 
+ */
+
+public class Die
+{
+    private int rollValue;
+
+    public Die()
+    {
+        rollValue = 0;
+    }
+
+    public int getRollValue()
+    {
+        return rollValue;
+    }
+
+    public int rollDie()
+    {
+        rollValue = (int) Math.random() * 7 + 1;
+        return rollValue;
+    }
+}
